89a9a3db700a50a99e2e6c669e5d63079bf7904f
Andrej Tashkovski <andrejtashkovski88@gmail.com>
2025-10-07 00:23:50 +0200

    Added the second sub task using Git. :)

---
+++ AV_1-2.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Zdravo, kako si?" << endl;
+    // dodadi go ovoj red
+    cout << "Neshto ne ti se pravi muabet?" << endl;
+    return 0;
+}
