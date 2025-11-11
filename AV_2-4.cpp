ca0674b9778431d167448e24cc2245240b096d33
Andrej Tashkovski <andrejtashkovski88@gmail.com>
2025-11-11 02:29:36 +0100

    Nemam so da kazam

---
+++ AV_2-4.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Ostatokot pri delenje na 19 so 2 e: " << 19 % 2 << endl;
+    cout << "Ostatokot pri delenje na 19 so 3 e: " << 19 % 3 << endl;
+    cout << "Ostatokot pri delenje na 19 so 5 e: " << 19 % 5 << endl;
+    cout << "Ostatokot pri delenje na 19 so 8 e: " << 19 % 8 << endl;
+    return 0;
+}
