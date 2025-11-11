940459026bd2ad0810ff472a770667d9476cb2f1
Andrej Tashkovski <andrejtashkovski88@gmail.com>
2025-11-11 02:42:07 +0100

    Commiting

---
+++ AV_2-5.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    float r;
+    cin >> r;
+    cout << "Perimetarot na krugot e: " << 2 * r * 3.14 << endl;
+    cout << "Ploshtinata na krugot e: " << r * r * 3.14 << endl;
+    return 0;
+}
