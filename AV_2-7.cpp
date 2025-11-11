364708549b4a9b3162c7a6129a55ffb7c11bd791
Andrej Tashkovski <andrejtashkovski88@gmail.com>
2025-11-11 03:07:28 +0100

    Bit shifting on a basic level

---
+++ AV_2-7.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+    cout << (char) ((int)c + 32) << endl;       //smth like bit shifting
+    return 0;
+}
