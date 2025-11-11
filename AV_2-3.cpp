b328673f6f90ae3ed9c1464c34f6b7621ccadc2e
Andrej Tashkovski <andrejtashkovski88@gmail.com>
2025-11-11 02:20:53 +0100

    Training with git

---
+++ AV_2-3.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    float a = 3, b = 5, c = 12;
+    cout << "Aritmetichkata sredina e: " << (a + b + c) / 3 << endl;
+    return 0;
+}
