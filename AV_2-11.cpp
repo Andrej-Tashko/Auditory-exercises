37c8797020f4c5097707491c5c1642d446257fb3
Andrej Tashkovski <andrejtashkovski88@gmail.com>
2025-11-11 03:30:45 +0100

    Final exercise from AV 2

---
+++ AV_2-11.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << "Data na ragjanje: " << n / 1000000 << "." << (n / 10000) % 100 << endl;
+    return 0;
+}
