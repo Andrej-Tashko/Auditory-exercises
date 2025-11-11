69bf855a1b3d10daf58595ced19ee8237e2efd54
Andrej Tashkovski <andrejtashkovski88@gmail.com>
2025-11-11 03:23:37 +0100

    Easy

---
+++ AV_2-10.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    cout << "Najznachajna cifra e: " << x / 100 << endl;
+    cout << "Najmalku znachajna cifra e: " << x % 10 << endl;
+    return 0;
+}
